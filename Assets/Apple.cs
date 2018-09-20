b3dc8b96029227dd38cb181aac6bd1531d8190b8
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-09-20 05:19:40 -0400

    Game playable but no complete
    As made in summary

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour {
+    [Header("Set in inspector")]
+
     public static float bottomY = -20f;
 	// Use this for initialization
 	void Start () {
 		
 	}
+    public void AppleDestoryed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach(GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
         }
 	}
 }

91b351f2e4fdfcd558d9cb565abeea6e0586d40d
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-09-20 00:11:30 -0400

    Apples go flying
    Apples are now being created

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -17,18 +17,34 @@ public class AppleTree : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Invoke("DropApple", 2f);
 	}
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
-        if(pos.x < -leftandRightEdge) {
+        if (pos.x < -leftandRightEdge) {
             speed = Mathf.Abs(speed);
         } else if (pos.x> leftandRightEdge ) {
             speed = -Mathf.Abs(speed);
         }
+       
 	}
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+
+    }
 }
